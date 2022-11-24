34c65043b1b58c19c2065b9fc94b04df67eeaf3d
sttaseen <sttaseen@gmail.com>
2022-11-24 14:42:59 +1300

    auto commit -fix auto-run

--- auto-run.py
+++ auto-run.py
@@ -1,8 +1,8 @@
 #!/bin/sh
 import os
 
-os.system('python downloadFromTxt.py)
-os.system('python extractFrames.py)
-os.system('python edit.py)
-os.system('python segment.py)
+os.system('python downloadFromTxt.py')
+os.system('python extractFrames.py')
+os.system('python edit.py')
+os.system('python segment.py')
 
